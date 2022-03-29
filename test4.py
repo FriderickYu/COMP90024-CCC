801032b51c70a2e47c41820d74ef1ab518ca5bbe
StevenNo1 <73212528+StevenNo1@users.noreply.github.com>
2022-03-29 22:36:47 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -158,7 +158,7 @@ if __name__ == '__main__':
         result_dict={}
         for i in middle_result_list:
             print('middle_result_list[i]', i)
-            if i != None:
+            if i != None and i != []:
                 [language, grid_id] = i
                 result_dict = sum_the_output(result_dict, language, grid_id)
         result_output(result_dict, language_dict)
